316bbccb9ce72f2f0d6040aead1ff4d2dca880d4
Shadrack Ngotho <shadrackngotho@gmail.com>
2019-07-03 10:51:58 +0300

    Handling of media clips on the UI Fetching of files from MySQL database. Improved some UI features.

--- app/src/main/java/com/pylanconsultancy/drama/Landing.java
+++ app/src/main/java/com/pylanconsultancy/drama/Landing.java
@@ -17,9 +17,31 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Landing extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Web api url to
+    public static final String DATA_URL = "http://127.0.0.1/movies.php";
+    public TextView sectionHeading;
+
+    //Data values to retrieve from Json.
+    public static final String TAG_Movie_Name = "PO_Movie";
+    public static final String TAG_Name = "PO_Name";
+    public static final String TAG_Advert ="PO_Advert";
+
+
+    //GridView Object
+    private GridView gridView;
+
+    //ArrayList for Storing video urls and titles
+    private static ArrayList<String> movies;
+    private static ArrayList<String> movie_url;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +49,23 @@ public class Landing extends AppCompatActivity
         setContentView(R.layout.activity_landing);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        sectionHeading =findViewById(R.id.sectionTitle);
+
+        gridView = findViewById(R.id.customgrid);
+
+        movies = new ArrayList<>();
+        movie_url = new ArrayList<>();
+
+
+        //Calling the getData method
+        getData();
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Please login to add a new movie", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Please login to add a new clip", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -45,6 +78,10 @@ public class Landing extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void getData(){
+
     }
 
     @Override
@@ -69,6 +106,7 @@ public class Landing extends AppCompatActivity
         switch (item.getItemId()) {
             case R.id.action_settings:
                 // User chose the "Settings" item, show the app settings UI...
+
                 return true;
 
             case R.id.action_search:
@@ -101,20 +139,41 @@ public class Landing extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_movies) {
-            // Handle the camera action
+            // Take user to movies page
+            setContentView(R.layout.activity_landing);
+            sectionHeading.setText(getString(R.string.moviesHeader));
+
         } else if (id == R.id.nav_poems) {
+            // Take user to Poems page
+            setContentView(R.layout.activity_poems);
+            sectionHeading.setText(getString(R.string.poemsHeader));
 
         } else if (id == R.id.nav_films) {
+            // Take user to Films page
+            setContentView(R.layout.activity_films);
+            sectionHeading.setText(getString(R.string.filmsHeader));
 
         } else if (id == R.id.nav_comedy) {
+            // Take user to Comedy page
+            setContentView(R.layout.activity_comedy);
+            sectionHeading.setText(getString(R.string.comedyHeader));
 
         } else if (id == R.id.nav_folkSongs) {
+            // Take user to Folk Songs UI
+            setContentView(R.layout.activity_folksongs);
+            sectionHeading.setText(getString(R.string.folksongHeader));
 
-        } else if (id == R.id.nav_folkDances) {
+        } else if (id == R.id.nav_dances) {
+            // Take user to Folk Dances page
+            setContentView(R.layout.activity_dances);
+            sectionHeading.setText(getString(R.string.dancesHeader));
 
         }  else if (id == R.id.nav_share) {
+            // Provide user with options through which they can share app.
+            //Set app url in Play Store in set text
 
         } else if (id == R.id.nav_rating) {
+            //Provide user with Google's rating UI or take user to Google Play store and focus on rating section
 
         }
 
